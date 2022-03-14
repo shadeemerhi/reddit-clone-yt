17c9a8fb9dc416b8cf15faa0f282a325da9b594c
shadeemerhi <shadmerhi@gmail.com>
2022-03-14 01:18:15 -0600

    fix wrapper padding

--- src/components/Community/ContentWrapper.tsx
+++ src/components/Community/ContentWrapper.tsx
@@ -6,12 +6,16 @@ type ContentWrapperProps = {};
 
 const ContentWrapper: React.FC<ContentWrapperProps> = () => {
   return (
-    <Flex justify="center" border="1px solid pink" p="16px">
+    <Flex justify="center" border="1px solid pink" p="16px 0px">
       <Flex width="95%" maxWidth="860px" border="1px solid red">
         {/* <Box width="65%" border="1px solid green"></Box> */}
         {/* Left Content */}
         <CreatePostLink />
-        <Box flexGrow={1} border="1px solid blue">
+        <Box
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border="1px solid blue"
+        >
           hello
         </Box>
 
--- src/components/Community/CreatePostLink.tsx
+++ src/components/Community/CreatePostLink.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import InputItem from "../Layout/InputItem";
-import { FaReddit } from "react-icons/fa";
-import { IoImageOutline } from "react-icons/io5";
-import { BsLink45Deg } from "react-icons/bs";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import React from "react";
+import { BsLink45Deg } from "react-icons/bs";
+import { FaReddit } from "react-icons/fa";
+import { IoImageOutline } from "react-icons/io5";
 
 type CreatePostProps = {};
 
