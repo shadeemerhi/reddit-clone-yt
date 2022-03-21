b233dfcf013891491a4a6046e673c4f911ae756d
shadeemerhi <shadmerhi@gmail.com>
2022-03-21 12:50:14 -0700

    cloud function to delete post comments on post delete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,3 +11,26 @@ export const createUserDocument = functions.auth
       .doc(user.uid)
       .set(JSON.parse(JSON.stringify(user)));
   });
+
+export const deletePostComments = functions.firestore
+  .document(`posts/{postId}`)
+  .onDelete(async (snap) => {
+    const postId = snap.id;
+    console.log("HERE IS POST ID", postId);
+
+    admin
+      .firestore()
+      .collection("comments")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          if (doc.data().postId === postId) {
+            console.log("DELETING COMMENT: ", doc.id, doc.data().text);
+            doc.ref.delete();
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("Error deleting post comments");
+      });
+  });
